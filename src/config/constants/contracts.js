c3d3e970d81b4eef548d2db4bdef90b892532e78
Eye Sage <eyessage2021@gmail.com>
2021-10-27 12:59:24 -1000

    new mainnet addresses

--- src/config/constants/contracts.js
+++ src/config/constants/contracts.js
@@ -1,7 +1,7 @@
 export default {
     stakingReward: {
         43113: "0x1e9e423656Af37156EecBf024EcEb2BCd8d0c04e",
-        43114: "0xea399B7354864B53C760d8B7c83086208767E1f9",
+        43114: "0x66f44675065694F703c659B585dEac9F08Eb9B03",
     },
     stakingRewardPGL: {
         43113: "0x9a9da259286cE944DC38F30C996F278FBD37501e",
@@ -9,10 +9,10 @@ export default {
     },
     stakingRewardSL3: {
         43113: "0x8162cccb5628e200869f34B8b068bBA70E80dBdA",
-        43114: "0xea399B7354864B53C760d8B7c83086208767E1f9", // Just using stakingReward above as placeholder
+        43114: "0x51bd8FA70dcA4f9A8b4a65e0E7a0D82b6281812C",
     },
     stakingRewardSL3PGL: {
         43113: "0x0b83Bf25F14518d5Edfd6C482d9D9fc93533559e",
-        43114: "0xD9c91b8aF2317a82E1986A8D3De687437Ead50Dd", // Just using stakingRewardPGL above as placeholder
+        43114: "0xcf6931015826E0b9Eef0B550685a39Dc53dDaAbf",
     }
 }
