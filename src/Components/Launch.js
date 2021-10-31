82d44f00a9895719a6a4e6abf23d65ea79b56210
Eye Sage <eyessage2021@gmail.com>
2021-10-31 12:54:14 -1000

    added favicon

--- src/Components/Launch.js
+++ src/Components/Launch.js
@@ -4,7 +4,7 @@ const Launch = ({ unlock }) => {
     return (
         <>
         <img src={background} className="LaunchScreen" alt="background" />
-        <button onClick={unlock} class="btn-hover color-1">Enter The IceAge</button>
+        <button onClick={unlock} className="btn-hover color-1">Enter The IceAge</button>
         </>
     );
 }
